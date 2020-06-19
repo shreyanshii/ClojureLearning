f5e494678caa8fcab0dd4d1fbdb419e5f5b2674c
shreyanshii <shreyanshibharadia7@gmail.com>
2020-06-19 16:02:24 +0530

    2

---
+++ clojure/series/src/series.clj
@@ -0,0 +1,18 @@
+(ns series)
+
+(defn slices [string length] ;; <- arglist goes here
+  ;; your code goes here
+  (if(> length (count string))
+	[]
+	(if (= length 0) ([""])
+		(let [ans []]
+			(loop[x 0]
+			(when (<= (+ length x) (count string))
+				(conj ans (subs string x (+ x length)))
+	
+				(recur (inc x))
+			))
+		ans)
+	)
+  )
+)
---
+++ clojure/word-count/src/word_count.clj
@@ -0,0 +1,15 @@
+(ns word-count)
+
+(defn word-count [s] ;; <- arglist goes here
+  ;; your code goes here
+ (dissoc (frequencies 
+           (clojure.string/split 
+	 (clojure.string/replace 
+		(clojure.string/replace 
+			(clojure.string/lower-case s) 
+			#"\s{2,}" " ") 
+		#"[\,\:\!\&\@\$\%\^\&]" " ") 
+	   #" ") 
+   )
+  "")
+)
