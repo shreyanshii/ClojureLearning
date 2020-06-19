7915e277ea8b1f57142885d38bb875632952489d
shreyanshii <shreyanshibharadia7@gmail.com>
2020-06-19 17:04:42 +0530

    3

---
+++ clojure/sublist/src/sublist.clj
@@ -0,0 +1,24 @@
+(ns sublist)
+
+(defn classify1 [list1 list2] ;; <- arglist goes here
+      ;; your code goes here
+	 (let [len (count list1)]
+			(some #(= list1 %)
+				(partition len 1 list2) )
+	  )
+)
+
+(defn classify [list1 list2] 
+	[list1 list2]
+  (if (= list1 list2)
+    :equal
+    (if (< (count list1) (count list2))
+      (if (classify1 list1 list2)
+        :sublist
+        :unequal)
+      (if (classify1 list2 list1)
+        :superlist
+        :unequal)))
+)
+
+
